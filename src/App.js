f21d0418ddc5ce1f1dc0cb96cda668b2146e6422
Olly Holovchenko <oleh.holo@gmail.com>
2022-08-06 23:36:04 +0100

    Main functionality done.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import { Container, Box, Typography, Button } from '@mui/material';
+import data from './Assets/Dice.json';
+import {useState, useEffect} from 'react'
 
-function App() {
+const App = () => {
+  const [rolledDice, setRolledDice] = useState([]);
+
+  const rollDice = () => {
+    let diceArray = []
+    data.dice.map((die) => {
+      const randomDie = die[Math.floor(Math.random()*die.length)]
+      diceArray.push(randomDie);
+    })
+    setRolledDice(diceArray)
+  }
+
+  useEffect(() => {
+    console.log('Rolled the dice!')
+    rollDice()
+  },[])
+
+
   return (
     <Container>
       <Box>
-        <Typography variant="h2" gutterBottom>
-          Hello World
+        <Typography variant="h3" gutterBottom>
+          tell me a story about...
         </Typography>
       </Box>
       <Box>
-        <Typography variant="h4" gutterBottom>
-          Dice go here
-        </Typography>
+        {rolledDice.map((die, i) => <Typography variant="h5" key={i} >{die}</Typography>)}
       </Box>
       <Box>
-        <Button>Roll</Button>
+        <Button onClick={rollDice}>Roll</Button>
       </Box>
     </Container>
   );
