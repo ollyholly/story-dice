59599a9160996d97dbf61d5297c8acaa2a6a4ea6
Olly Holovchenko <oleh.holo@gmail.com>
2022-08-07 00:17:57 +0100

    Finish MVP. Add more icons.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,71 @@
-import { Container, Box, Typography, Button } from '@mui/material';
+import { Container, Box, Typography, Button, Card } from '@mui/material';
 import data from './Assets/Dice.json';
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    blacky: {
+      main: '#222222',
+      contrastText: '#fff'
+    }
+  }
+});
 
 const App = () => {
   const [rolledDice, setRolledDice] = useState([]);
 
   const rollDice = () => {
-    let diceArray = []
+    let diceArray = [];
     data.dice.map((die) => {
-      const randomDie = die[Math.floor(Math.random()*die.length)]
+      const randomDie = die[Math.floor(Math.random() * die.length)];
       diceArray.push(randomDie);
-    })
-    setRolledDice(diceArray)
-  }
+    });
+    setRolledDice(diceArray);
+  };
 
   useEffect(() => {
-    console.log('Rolled the dice!')
-    rollDice()
-  },[])
-
+    console.log('Rolled the dice!');
+    rollDice();
+  }, []);
 
   return (
     <Container>
-      <Box>
-        <Typography variant="h3" gutterBottom>
-          tell me a story about...
-        </Typography>
-      </Box>
-      <Box>
-        {rolledDice.map((die, i) => <Typography variant="h5" key={i} >{die}</Typography>)}
-      </Box>
-      <Box>
-        <Button onClick={rollDice}>Roll</Button>
-      </Box>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Typography variant="h3" gutterBottom my={5} align="center">
+            tell me a story about...
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            alignItems: 'flex-start',
+            alignContent: 'center',
+            justifyContent: 'center'
+          }}
+          mb={5}
+        >
+          {rolledDice.map((die, i) => (
+            <Box m={1} key={i}>
+              <Card variant="outlined">
+                <Box p={2}>
+                  <Typography variant="h3">{die}</Typography>
+                </Box>
+              </Card>
+            </Box>
+          ))}
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Button onClick={rollDice} variant="contained" color="blacky" size="large">
+            roll me
+          </Button>
+        </Box>
+      </ThemeProvider>
     </Container>
   );
-}
+};
 
 export default App;
